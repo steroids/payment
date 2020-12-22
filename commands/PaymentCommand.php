13286b6e91589550720464120e92f290d73627ed
Vladimir Kozhin <hello@kozhindev.com>
2020-12-22 17:41:36 +0700

    add callback console action

--- commands/PaymentCommand.php
+++ commands/PaymentCommand.php
@@ -5,6 +5,7 @@ namespace steroids\payment\commands;
 use steroids\core\structure\RequestInfo;
 use steroids\payment\enums\PaymentStatus;
 use steroids\payment\models\PaymentOrder;
+use steroids\payment\models\PaymentProviderLog;
 use steroids\payment\structure\PaymentProcess;
 use yii\console\Controller;
 use yii\console\Exception;
@@ -44,6 +45,7 @@ class PaymentCommand extends Controller
             ]);
 
             echo "\nAvailable commands:\n";
+            echo "\t- php yii payment/orders 999 callback\n";
             echo "\t- php yii payment/orders 999 success\n";
             echo "\t- php yii payment/orders 999 failure\n";
         } else {
@@ -54,16 +56,26 @@ class PaymentCommand extends Controller
                     'orderId' => $order->getId(),
                 ]
             ]);
-            $process = new PaymentProcess();
             switch ($action) {
                 case 'success':
+                    $process = new PaymentProcess();
                     $process->newStatus = PaymentStatus::SUCCESS;
                     $order->end($request, $process);
                     break;
 
                 case 'failure':
+                    $process = new PaymentProcess();
                     $process->newStatus = PaymentStatus::FAILURE;
                     $order->end($request, $process);
+                    break;
+
+                case 'callback':
+                    if (!$order->lastProviderLogger || $order->lastProviderLogger->callMethod !== 'callback') {
+                        echo "No callback logs\n";
+                        return;
+                    }
+
+                    $order->callback(RequestInfo::createFromRaw($order->lastProviderLogger->requestRaw));
                     break;
 
                 default:
