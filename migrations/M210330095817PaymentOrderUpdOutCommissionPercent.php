3efe6fdfb41e513bbea46d794f3e33942b6e7c5d
Семён Зверев <drophere.box@gmail.com>
2021-03-31 12:15:42 +0700

    fix migrations

--- migrations/M210330095817PaymentOrderUpdOutCommissionPercent.php
+++ migrations/M210330095817PaymentOrderUpdOutCommissionPercent.php
@@ -10,32 +10,10 @@ class M210330095817PaymentOrderUpdOutCommissionPercent extends Migration
     public function safeUp()
     {
         $this->alterColumn('payment_orders', 'outCommissionPercent', $this->decimal(19,2));
-
-        $orders = (new Query())
-            ->from('payment_orders')
-            ->all();
-
-        foreach ($orders as $order){
-            if($order['outCommissionPercent'] === 0 || !$order['outCommissionPercent']){
-                continue;
-            }
-            $this->update('payment_orders', ['outCommissionPercent' => $order['outCommissionPercent'] / 100], ['id' => $order['id']]);
-        }
     }
 
     public function safeDown()
     {
-        $orders = (new Query())
-            ->from('payment_orders')
-            ->all();
-
-        foreach ($orders as $order){
-            if($order['outCommissionPercent'] === 0 || !$order['outCommissionPercent']){
-                continue;
-            }
-            $this->update('payment_orders', ['outCommissionPercent' => $order['outCommissionPercent'] * 100], ['id' => $order['id']]);
-        }
-
         $this->alterColumn('payment_orders', 'outCommissionPercent', $this->integer());
     }
 }
--- migrations/M210330103409PaymentMethodUpdOutCommissionPercent.php
+++ migrations/M210330103409PaymentMethodUpdOutCommissionPercent.php
@@ -10,32 +10,10 @@ class M210330103409PaymentMethodUpdOutCommissionPercent extends Migration
     public function safeUp()
     {
         $this->alterColumn('payment_methods', 'outCommissionPercent', $this->decimal(19,2));
-
-        $orders = (new Query())
-            ->from('payment_methods')
-            ->all();
-
-        foreach ($orders as $order){
-            if($order['outCommissionPercent'] === 0 || !$order['outCommissionPercent']){
-                continue;
-            }
-            $this->update('payment_methods', ['outCommissionPercent' => $order['outCommissionPercent'] / 100], ['id' => $order['id']]);
-        }
     }
 
     public function safeDown()
     {
-        $orders = (new Query())
-            ->from('payment_methods')
-            ->all();
-
-        foreach ($orders as $order){
-            if($order['outCommissionPercent'] === 0 || !$order['outCommissionPercent']){
-                continue;
-            }
-            $this->update('payment_methods', ['outCommissionPercent' => $order['outCommissionPercent'] * 100], ['id' => $order['id']]);
-        }
-
         $this->alterColumn('payment_methods', 'outCommissionPercent', $this->integer());
     }
 }
